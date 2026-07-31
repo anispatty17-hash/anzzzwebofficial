d1b4231234e261559b4247f356989f4ed9c46d1b
anzzzsenpai <anispatty17@gmail.com>
2026-07-31 11:28:45 +0900

    Update social.ts

--- src/lib/social.ts
+++ src/lib/social.ts
@@ -3,11 +3,11 @@ export const socialLinks = {
   whatsapp: "https://wa.me/6281234567890",
   discord: "https://discord.com/invite/example",
   instagram: "https://instagram.com",
-  youtube: "https://youtube.com",
-  github: "https://github.com",
-  tiktok: "https://tiktok.com",
-  facebook: "https://facebook.com",
-  telegram: "https://t.me/example",
+  youtube: "https://youtube.com/@4nzzz2003?si=suH8_B7SaugBYHoE",
+  github: "https://github.com/anispatty17-hash",
+  tiktok: "tiktok.com/@shiinamahiru10",
+  facebook: "https://www.facebook.com/share/1CNJPN919t/",
+  telegram: "https://t.me/anzzzsenpai",
 } as const;
 
 export type SocialKey = keyof typeof socialLinks;
